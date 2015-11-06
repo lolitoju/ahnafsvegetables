05ff02e59eb59efc076d6409c96c22f48b0b1efe
andy-1x-5x <andy-1x-5x@hotmail.com>
2015-11-06 01:07:54 -0500

    Fixed Controller.php

--- mvc/app/core/Controller.php
+++ mvc/app/core/Controller.php
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-<?php
-	class Controller {
-		public function model($model) {
-			require_once 'app/models/' . $model . '.php';
-			return new $model();
-		}
-	}
-?>
-=======
 <?php
 	class Controller {
 		public function model($model) {
@@ -15,6 +5,4 @@
 			return new $model();
 		}
 	}
-	}
 ?>
->>>>>>> fe06a12df8a2fa4d5fd4a93042b879e5163d49b2
